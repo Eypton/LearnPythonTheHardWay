50d884def84d47c60ebeba0849b30119e3070227
Stanley H Griggs II <shg5004@gmail.com>
2016-01-27 16:33:36 -0500

    Completed Exercise 12

--- ex12.py
+++ ex12.py
@@ -5,3 +5,10 @@ height = raw_input("How tall are you? ")
 weight = raw_input("How much do you weigh? ")
 
 print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+
+#LearnPythonTheHardWay git:(master) ✗ python ex13.py Dog Pig Goat
+#The script is called: ex13.py
+#Your first variable is: Dog
+#Your second variable is: Pig
+#Your third variable is: Goat
